1d26a4c9a27389b29220ce3f917ac3848a308963
Jediael Andreassa <jedi.andreassa@gmail.com>
2025-06-24 01:06:42 -0300

    update
    repo criado para o site que estava em uma máquina local

---
+++ calcas.php
@@ -0,0 +1,73 @@
+<?php
+include "includes/cabecalho.php";
+include "includes/banner.php";
+?>
+
+    <div class="categorias">
+        <h2 class="titulo">CALÇAS</h2>
+    </div>
+
+    <div class="produtos">
+        <h2 class="titulo">Produtos que estão bombando!</h2>
+        <div class="lista-produtos">
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+            <div class="item-produto">
+                <img src="images/calca.png" class="foto-produto">
+                <h3 class="titulo-produto">Calça Alfaiataria</h3>
+                <p class="descricao-produto">Modelo Wide Leg alfaiataria em linho. Uma peça pra vida toda!</p>
+                <h3 class="preco-produto">R$ 180,00</h3>
+                <a href="#" class="botao-produto">Ver mais</a>
+            </div>
+           
+        </div>
+    </div>
+
+    <?php
+    include "includes/facilidades.php";
+    ?>
+
+    <?php
+    include "includes/newsletter.php";
+    ?>
+
+    <?php
+    include "includes/rodape.php";
+    ?>
+
+    <?php
+    include "includes/script.php";
+    ?>
---
+++ produto.php
@@ -0,0 +1,19 @@
+<?php
+include "includes/cabecalho.php";
+include "includes/categorias.php";
+?>
+
+<div class="produtos" style="text-align: center;">
+    <h2 class="titulo">Camiseta Conforto</h2>
+    <img src="images/camiseta.png" class="foto-produto">
+    <div class="informacoes-produto">
+        <p class="descricao-produto">Multicores e tamanhos. Tecido de algodão 100%, fresquinho para o verão. Modelagem unissex.</p>
+        <h3 class="preco-produto"><mark>R$ 70,00</mark></h3>
+    </div>
+</div>
+
+<?php
+include "includes/facilidades.php";
+include "includes/newsletter.php";
+include "includes/rodape.php";
+?>
